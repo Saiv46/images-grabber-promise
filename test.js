f78f5029e8fb47101dbabab41043d242b7f2ed84
Alexander Ivanov <47699394+Saiv46@users.noreply.github.com>
2021-01-27 17:22:30 +0800

    Update test.js

--- test.js
+++ test.js
@@ -30,31 +30,38 @@ function testReddit(name, url, ...funcs) {
 
 (async () => {
 	const is = {
-		link: a => ok(a.every(v => { try { new URL(v); return true } catch (e) { return false } }), 
-		jpg: a => is.link(a) && ok(a.every(v => new URL(v).pathname.endsWith(".jpg"))),
-		png: a => is.link(a) && ok(a.every(v => new URL(v).pathname.endsWith(".png")))
+		link: new URL(v),
+		linkSafe: v => { try { is.link(v); return true } catch (e) { return false } },
+		jpg: v => is.link(v) && ok(new URL(v).pathname.endsWith(".jpg"), `URL path "${v}" not ends with .jpg`),
+		png: v => is.link(v) && ok(new URL(v).pathname.endsWith(".png"), `URL path "${v}" not ends with .png`),
+		samePath: (a, b) => equal(is.linkSafe(a) ? new URL(a).pathname : a, is.linkSafe(b) ? new URL(b).pathname : b)
 	};
+	is.array = {};
+	for (const name of is) {
+		const func = is[name];
+		if (typeof func !== 'function') continue;
+		is.array[name] = a => {
+			ok(Array.isArray(a), "Result is not an array");
+			a.forEach(v => func(v));
+		};
+	}
 
-	await testReddit("full link", "https://www.reddit.com/r/DDLC/comments/8c8k2v/only_js_function_which_works_correctly/", is.png);
-	await testReddit("short link", "https://redd.it/8kimm3", is.png);
+	await testReddit("full link", "https://www.reddit.com/r/DDLC/comments/8c8k2v/only_js_function_which_works_correctly/", is.array.png);
+	await testReddit("short link", "https://redd.it/8kimm3", is.array.png);
 
-	await testVK("wall", "https://vk.com/wall-168965593_1604", is.jpg, w => {
+	await testVK("wall", "https://vk.com/wall-168965593_1604", is.array.jpg, w => {
 		[
 			"/c857632/v857632531/173335/iUlssMo6Fn8.jpg",
 			"/c857632/v857632531/17333f/Nq76kMBKtmc.jpg",
 			"/c857632/v857632531/173349/vbU7C8ujb7Y.jpg"
-		].every((v, i) => equal(new URL(w[i]).pathname, v))
+		].every((v, i) => is.samePath(w[i], v))
 	}
 	);
-	await testVK("photo", "https://vk.com/photo1_456256890", is.jpg, ([ p ]) => 
-		equal(new URL(p).pathname, "/c639230/v639230001/fabe/yU0Q2Knm9PY.jpg")
-	);
-	await testVK("album", "https://vk.com/album393055078_0", is.jpg);
+	await testVK("photo", "https://vk.com/photo1_456256890", is.array.jpg, ([ p ]) => is.samePath(p, "/c639230/v639230001/fabe/yU0Q2Knm9PY.jpg"));
+	await testVK("album", "https://vk.com/album393055078_0", is.array.jpg);
 	
 	try {
-		await testVK("doc throws", "https://vk.com/doc393055078_536126416", ([ p ]) => 
-			equal(new URL(p).pathname, "/c848216/u76071059/docs/d17/93b993ab8078/gachiteam.gif")
-		);
+		await testVK("doc throws", "https://vk.com/doc393055078_536126416", ([ p ]) => is.samePath(p, "/c848216/u76071059/docs/d17/93b993ab8078/gachiteam.gif"));
 		throw new AssertionError("doc should throw with service token");
 	} catch {
 		console.log(`VK doc throw OK`)
